a82be06f01a3d4a7a3fee8470126d46d20e9f931
Sophia Kleist Karlson <54801990+SophiaKleistKarlson@users.noreply.github.com>
2021-05-10 11:10:55 +0200

    Update create_image_search_venv.sh

--- assignment_2/create_image_search_venv.sh
+++ assignment_2/create_image_search_venv.sh
@@ -1,1 +1,18 @@
+#!/usr/bin/env bash
 
+VENVNAME=image_search_venv 
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+pip install opencv-python
+
+python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install requirements.txt
+
+deactivate
+echo "build $VENVNAME"
