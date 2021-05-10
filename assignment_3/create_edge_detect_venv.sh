fb6db969190707b2a63b879fe166e666f9e4d197
Sophia Kleist Karlson <54801990+SophiaKleistKarlson@users.noreply.github.com>
2021-05-10 11:24:00 +0200

    Update create_edge_detect_venv.sh

--- assignment_3/create_edge_detect_venv.sh
+++ assignment_3/create_edge_detect_venv.sh
@@ -1,4 +1,18 @@
-pandas == 1.1.5
-cv2 == 4.5.1
-numpy == 1.19.5
-matplotlib == 3.3.4
+#!/usr/bin/env bash
+
+VENVNAME=edge_detect_venv 
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+pip install opencv-python
+
+python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install requirements.txt
+
+deactivate
+echo "build $VENVNAME"
