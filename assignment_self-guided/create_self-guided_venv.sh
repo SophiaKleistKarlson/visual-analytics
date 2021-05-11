69a4e74e7302e677ea539011a87a00ad7fb6fe1d
Sophia Kleist Karlson <54801990+SophiaKleistKarlson@users.noreply.github.com>
2021-05-11 10:22:26 +0200

    Update create_self-guided_venv.sh

--- assignment_self-guided/create_self-guided_venv.sh
+++ assignment_self-guided/create_self-guided_venv.sh
@@ -1,1 +1,25 @@
+  
+#!/usr/bin/env bash
 
+#if in worker2, first set the working directory 
+#cd cds-language/src
+
+VENVNAME=keyword_sentiment_environment
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+
+# then run the script: (in worker2 , it wouldn't be in the "src" folder, because the requirements and scripts are in the same folder)
+#python3 src/keyword_headline_sentiment.py 
+
+#then deactivate the environment
+deactivate
+echo "build $VENVNAME"
